519e2371c5c14ef54bb64fd8c396443a468e8fab
betty Domain <betty.domain@free.fr>
2021-02-08 16:22:21 +0100

    initial commit with external librarie RewardsCentral, and configuration of build.gradle

---
+++ src/main/java/tourguide/rewardscentral/RewardscentralApplication.java
@@ -0,0 +1,13 @@
+package tourguide.rewardscentral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardscentralApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RewardscentralApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tourguide/rewardscentral/RewardscentralApplicationTests.java
@@ -0,0 +1,13 @@
+package tourguide.rewardscentral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardscentralApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
